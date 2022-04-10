51ecbfe9fcddaa2e34211c19da77e14139ba0b14
RoniShulmann <ronishulman@gmail.com>
2022-04-10 17:13:06 +0300

    update

---
+++ B22 Ex02 Amir 208423491 Roni 322437815/B22 Ex02 Amir 208423491 Roni 322437815/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace B22_Ex02_Amir_208423491_Roni_322437815
+{
+    class Program
+    {
+        //hello amirush
+    }
+}
