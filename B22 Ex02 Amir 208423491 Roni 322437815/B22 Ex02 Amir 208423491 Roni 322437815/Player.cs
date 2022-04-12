a460fc0ca8cf9d6ba322fb3922e874b2e759b8cc
AmirAnckonina <50033620+AmirAnckonina@users.noreply.github.com>
2022-04-12 17:50:28 +0300

    Class Building

---
+++ B22 Ex02 Amir 208423491 Roni 322437815/B22 Ex02 Amir 208423491 Roni 322437815/Player.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace B22_Ex02_Amir_208423491_Roni_322437815
+{
+    public class Player
+    {
+        private string m_Name; //Max size of 20
+        private int m_NumOfDiscs;
+        private readonly char m_DiscType;
+        private ePlayerType m_PlayerType;
+
+        public Player(string i_Name, int i_NumOfDiscs, char i_DiscType, ePlayerType i_PlayerType)
+        {
+            m_Name = i_Name;
+            m_NumOfDiscs = i_NumOfDiscs;
+            m_PlayerType = i_PlayerType;
+            m_DiscType = i_DiscType;
+        }
+
+        public string Name
+        {
+            get { return m_Name; }
+            set { m_Name = value; }
+        }
+
+        public int NumOfDiscs
+        {
+            get { return m_NumOfDiscs; }
+            set { m_NumOfDiscs = value; }
+        }
+
+        public ePlayerType PlayerType
+        {
+            get { return m_PlayerType; }
+            set { m_PlayerType = value; }
+        }
+
+        public char DiscType()
+        {
+            return m_DiscType;
+        }
+
+    }
+}
