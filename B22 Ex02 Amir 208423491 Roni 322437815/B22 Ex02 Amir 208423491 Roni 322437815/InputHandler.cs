07e08ce85a489c0e1388698aaa769e5235e268a1
AmirAnckonina <50033620+AmirAnckonina@users.noreply.github.com>
2022-04-14 16:42:36 +0300

    AmirUpdates

--- B22 Ex02 Amir 208423491 Roni 322437815/B22 Ex02 Amir 208423491 Roni 322437815/InputHandler.cs
+++ B22 Ex02 Amir 208423491 Roni 322437815/B22 Ex02 Amir 208423491 Roni 322437815/InputHandler.cs
@@ -5,21 +5,22 @@ using System.Text;
 
 namespace B22_Ex02_Amir_208423491_Roni_322437815
 {
-    class InputHandler
+    public class InputHandler
     {
         StringBuilder m_RawInput;
-        bool m_IsInputValid;
-        int[] m_FromIndicies;
-        int[] m_ToIndicies;
+        bool m_InputStructureIsValid;
+        int[] m_FromIndex;
+        int[] m_ToIndex;
 
         /*Square m_FromSquare;
         Square m_ToSquare;*/
 
         public InputHandler()
         {
-            m_FromIndicies = new int[2];
-            m_ToIndicies = new int[2];
-            m_IsInputValid = false;
+            m_RawInput = new StringBuilder();
+            m_FromIndex = new int[2];
+            m_ToIndex = new int[2];
+            m_InputStructureIsValid = false;
         }
 
         public StringBuilder RawInput
@@ -28,17 +29,119 @@ namespace B22_Ex02_Amir_208423491_Roni_322437815
             set { m_RawInput = value; }
         }
 
-        public bool GetIsInputValid()
+        public bool InputStructureIsValid()
         {
-            return m_IsInputValid;
+            return m_InputStructureIsValid;
         }
 
-        public bool InputValidation()
+        public void LoadNewInput(StringBuilder i_RawInput)
         {
-
-            return m_IsInputValid;
+            ClearPreviousInput();
+            m_RawInput = i_RawInput;
+            InputStructureValidation();
+            if (m_InputStructureIsValid)
+            {
+                UpdateIndicies();
+            }
         }
 
+        public void ClearPreviousInput()
+        {
+            m_RawInput.Clear();
+            m_FromIndex.Initialize();
+            m_ToIndex.Initialize();
+            m_InputStructureIsValid = false;
+        }
+
+        public void UpdateIndicies()
+        {
+            m_FromIndex[0] = LetterToNumberIndexConverter(m_RawInput[0]);
+            m_FromIndex[1] = LetterToNumberIndexConverter(m_RawInput[1]);
+            m_ToIndex[0] = LetterToNumberIndexConverter(m_RawInput[3]);
+            m_ToIndex[1] = LetterToNumberIndexConverter(m_RawInput[4]);
+        }
+
+        public void InputStructureValidation()
+        {
+            //CARFUL: validate if the first function returning false then the others won't be called.
+            if (LengthValidation() && MovingFromValidation() && MovingToValidation() && OperatorValidation())
+            {
+                m_InputStructureIsValid = true;
+            }
+
+            else
+            {
+                m_InputStructureIsValid = false;
+            }
+        }
+
+        public bool MovingFromValidation()
+        {
+            bool movingFromIndexIsValid;
+            
+            if (Char.IsUpper(m_RawInput[0]) && Char.IsLower(m_RawInput[1]))
+            {
+                movingFromIndexIsValid = true;
+            }
+
+            else
+            {
+                movingFromIndexIsValid = false;
+            }
+
+            return movingFromIndexIsValid;
+        }
+
+        public bool MovingToValidation()
+        {
+            bool movingToIndexIsValid;
+
+            if (Char.IsUpper(m_RawInput[3]) && Char.IsLower(m_RawInput[4]))
+            {
+                movingToIndexIsValid = true;
+            }
+
+            else
+            {
+                movingToIndexIsValid = false;
+            }
+
+            return movingToIndexIsValid;
+        }
+
+        public bool OperatorValidation()
+        {
+            bool operatorIsValid;
+
+            if (m_RawInput[2] == '>')
+            {
+                operatorIsValid = true;
+            }
+
+            else
+            {
+                operatorIsValid = false;
+            }
+
+            return operatorIsValid;
+        }
+
+        public bool LengthValidation()
+        {
+            bool lengthIsValid;
+
+            if (m_RawInput.Length == 5)
+            {
+                lengthIsValid = true;
+            }
+
+            else
+            {
+                lengthIsValid = false;
+            }
+
+            return lengthIsValid;
+        }
 
         public int LetterToNumberIndexConverter(char i_Letter)
         {
