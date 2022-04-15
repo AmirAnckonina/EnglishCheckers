da1f9276086df8a10abf739ce369d1396b905f4c
RoniShulmann <ronishulman@gmail.com>
2022-04-15 13:07:56 +0300

    updateBoard

--- B22 Ex02 Amir 208423491 Roni 322437815/B22 Ex02 Amir 208423491 Roni 322437815/Program.cs
+++ B22 Ex02 Amir 208423491 Roni 322437815/B22 Ex02 Amir 208423491 Roni 322437815/Program.cs
@@ -9,9 +9,16 @@ namespace B22_Ex02_Amir_208423491_Roni_322437815
     {
         public static void Main()
         {
+            int res;
+            bool Res;
             Board board1 = new Board(8);
             board1.InitializeBoard();
             board1.PrintBoard();
+            res= board1.GetDiscOccurences(eDiscType.XDisc);
+            Console.WriteLine("number of x: {0}", res);
+            Res= board1.IsIndexValid(2, 8);
+            Console.WriteLine("is index valid? : {0}", Res);
+            
         }
     }
 }
