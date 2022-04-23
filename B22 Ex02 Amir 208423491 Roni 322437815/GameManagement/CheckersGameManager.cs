c1556415455a85e9289a8977cdcc395d268b3e1b
RoniShulmann <ronishulman@gmail.com>
2022-04-23 15:19:17 +0300

    update Roni23Apr

--- B22 Ex02 Amir 208423491 Roni 322437815/GameManagement/CheckersGameManager.cs
+++ B22 Ex02 Amir 208423491 Roni 322437815/GameManagement/CheckersGameManager.cs
@@ -69,6 +69,7 @@ namespace GameManagement
                 RecurringTurnProcedure();
             }
 
+            m_Game.
             /// Winner Calaculation
             /// ScoreCalculation
         }
